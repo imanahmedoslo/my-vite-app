0caacf1d4c5d152e5cb5902657941cd0a401ab61
imanahmedoslo <imanahmedimanahmed@gmail.com>
2024-03-06 09:49:45 +0100

    fix typo

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -4,7 +4,7 @@ export default function SideBar(){
     return (<div style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
     <p style={{fontSize:"25px"}} className="fontSmall">KontaktInfo<b style={{color:'#049B79'}}>.</b></p>
     <p style={{fontSize:"25px"}} className="fontSmall">Dashboard<b style={{color:'#049B79'}}>.</b></p>
-    <p style={{fontSize:"25px"}} className="fontSmall">Omg meg<b style={{color:'#049B79'}}>.</b></p>
+    <p style={{fontSize:"25px"}} className="fontSmall">Om meg<b style={{color:'#049B79'}}>.</b></p>
     <p style={{fontSize:"25px"}} className="fontSmall">Prosjekter<b style={{color:'#049B79'}}>.</b></p>
     <p style={{fontSize:"25px"}} className="fontSmall">LinkedIn<b style={{color:'#049B79'}}>.</b></p>
     </div>)
